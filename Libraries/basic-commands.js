ea306c549a8f9c8c63cfc0b9f07f88abb9d1ced8
Sercan Muhlacı <sercan.620@gmail.com>
2018-04-08 00:11:32 +0300

    Version 1.3.4 Update.
    SendNotification method added.

--- Libraries/basic-commands.js
+++ Libraries/basic-commands.js
@@ -19,38 +19,34 @@ async function RunCommand(msg, channel)
         msg.delete(REPLY_DURATION);
         if (msg.member.permissions.has('MANAGE_ROLES')) {
             if (input.length === 0) {
-                //C#'da yazsam burayı methodun içerisine koymakla kalmaz bu tür methodları yazdığım bir sınıf oluştururdum.
-                //Java'da nasıl oluyor çözemedim -Sercan.
 
                 let roleList = await AllowedRoles.findAll({ attributes: ['name'] });
 
                 if (roleList.length < 1) {
-                    channel.send('No role set.').then(msg => msg.delete(REPLY_DURATION));
+                    SendNotification('Could not find any role. Contact with admin.');
                     return;
                 }
 
                 let result = 'Allowed roles are: ';
-                result += roleList.map(t => t.name = `@${t.name}`).join(', ') + ".";
+                result += roleList.map(t => t.name).join(', ') + ".";
 
-                channel.send(result).then(msg => msg.delete(REPLY_DURATION));
+                SendNotification(result, channel);
             }
             else if (input[0] === "add") {
                 let roleInput = input[1];
                 let selectedRole = getRole(roleInput, channel);
 
                 if (selectedRole == null) {
-                    channel.send("You didn't choose any Role.").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    )
+                    SendNotification("You didn't choose any Role.")
                     return;
                 }
 
                 // INSERT INTO ...
                 AllowedRoles.create({ roleId: selectedRole.id, name: selectedRole.name })
-                    .then(() => { channel.send("The role is now allowed.").then(msg => msg.delete(REPLY_DURATION)) })
+                    .then(() => { SendNotification("The role is now allowed.")})
                     .catch((e) => {
                         if (e.name === "SequelizeUniqueConstraintError")
-                            channel.send("That role already exists.").then(msg => msg.delete(REPLY_DURATION));
+                            SendNotification("That role already exists.");
                         else {
                             console.error(e);
                             channel.send(e);
@@ -62,22 +58,20 @@ async function RunCommand(msg, channel)
                 let selectedRole = getRole(roleInput, channel);
 
                 if (selectedRole == null) {
-                    channel.send("You didn't choose any Role.").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    )
+                    SendNotification("You didn't choose any Role.");
                     return;
                 }
 
                 // DELETE FROM . . .
                 let rowCount = await AllowedRoles.destroy({ where: { roleId: selectedRole.id } })
                 console.log(rowCount);
-                if (rowCount) channel.send("Role removed.").then(msg => msg.delete(REPLY_DURATION));
-                else channel.send("That role did not exist.").then(msg => msg.delete(REPLY_DURATION));
+                if (rowCount) SendNotification("Role removed.");
+                else SendNotification("That role did not exist.");
             }
-            else channel.send("Invalid command.").then(msg => msg.delete(REPLY_DURATION));
+            else SendNotification("Invalid command.");
 
         }
-        else channel.send("You don't have permission to manage roles.").then(msg => msg.delete(REPLY_DURATION));
+        else SendNotification("You don't have permission to manage roles.");
     }
 
     else if (commandName === "role") {
@@ -87,18 +81,16 @@ async function RunCommand(msg, channel)
         let roleList = await AllowedRoles.findAll({ attributes: ['roleId', 'name'] });
 
         if (input.length == 0) {
-            //C#'da yazsam burayı methodun içerisine koymakla kalmaz bu tür methodları yazdığım bir sınıf oluştururdum.
-            //Java'da nasıl oluyor çözemedim -Sercan.
 
             if (roleList.length < 1) {
-                channel.send('No role set.').then(msg => msg.delete(REPLY_DURATION));
+                SendNotification('Could not find any role. Contact with admin.');
                 return;
             }
 
             let result = 'Allowed roles are: ';
-            result += roleList.map(t => t.name = `@${t.name}`).join(', ') + ".";
+            result += roleList.map(t => t.name = `**${t.name}**`).join(', ') + ".";
 
-            channel.send(result).then(msg => msg.delete(REPLY_DURATION));
+            SendNotification(result);
         } else
 
             if (input[0] === "add") {
@@ -106,50 +98,38 @@ async function RunCommand(msg, channel)
                 let selectedRole = getRole(roleInput, channel);
 
                 if (selectedRole == null)
-                    channel.send("You didn't choose any Role.").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    )
+                    SendNotification("You didn't choose any Role.");
                 else if (msg.member.roles.exists("id", selectedRole.id))
-                    channel.send("You already have this role.").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    )
+                    SendNotification("You already have this role.");
                 else if (roleList.map(t => t.roleId).includes(selectedRole.id)) {
                     msg.member.addRole(selectedRole).then(
-                        channel.send("You just took the role! :kissing_heart:").then(
-                            msg => msg.delete(REPLY_DURATION)
-                        )
+                        SendNotification("You just took the role! :kissing_heart:")
                     );
                 }
-                else channel.send("You can't take this role :thumbsdown:").then(msg => msg.delete(REPLY_DURATION));
+                else SendNotification("You can't take this role :thumbsdown:");
 
             } else if (input[0] === "remove") {
                 let roleInput = input[1];
                 let selectedRole = getRole(roleInput, channel);
 
                 if (selectedRole == null)
-                    channel.send("Are you trying to remove nothing? :frowning:");
+                    SendNotification("Are you trying to remove nothing? :frowning:");
                 else if (!msg.member.roles.exists("id", selectedRole.id))
-                    channel.send("Do not worry, you already don't have this role.").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    )
+                    SendNotification("Do not worry, you already don't have this role.");
                 else if (roleList.map(t => t.roleId).includes(selectedRole.id)) {
                     msg.member.removeRole(selectedRole).then(
-                        channel.send("You've removed your role! :kissing_heart:").then(
-                            msg => msg.delete(REPLY_DURATION)
-                        )
+                        SendNotification("You've removed your role! :kissing_heart:")
                     );
                 }
                 else
-                    channel.send("You can't remove this role :thumbsdown:").then(
-                        msg => msg.delete(REPLY_DURATION)
-                    );
+                    SendNotification("You can't remove this role :thumbsdown:");
             }
-            else channel.send("Invalid command.").then(msg => msg.delete(REPLY_DURATION));
+            else SendNotification("Invalid command.");
 
     }
 
     else if (commandName === "help") {
-        channel.send("Check out your PM.").then(msg => msg.delete(REPLY_DURATION));
+        SendNotification("Check out your PM.");
         helpCommands(msg.member.user);
         msg.delete(REPLY_DURATION);
     }
@@ -167,22 +147,28 @@ async function RunCommand(msg, channel)
     }
 
     else if (commandName === "kick") {
-
         if (msg.member.roles.exists("id", '288244917367734273')) {
             let userToKick = msg.mentions.members.first();
             if (userToKick == null) {
-                channel.send('Command usage: !kick [member]');
+                SendNotification('Command usage: !kick [member]');
             } else {
+                msg.delete(REPLY_DURATION);
+                SendNotification("ERROR: Check Console & Contact with Admin.")
                 userToKick.kick().catch(console.error);
             }
         } else {
-            channel.send('You don\'t have permission to do that.')
-            console.log(msg.member.roles)
+            msg.delete(REPLY_DURATION);
+            SendNotification('You don\'t have permission to do that.');
         }
     }
 
     else if (commandName === "github") {
         channel.send("Check it out: https://github.com/smuhlaci/Rector-Bot");
+    }
+
+    function SendNotification(message)
+    {
+        channel.send(message).then(msg => msg.delete(REPLY_DURATION));
     }
 }
 
@@ -206,10 +192,10 @@ function helpCommands(user) {
             "  **!pig** -- pig?\n" +
             "  **!help** -- Guess what?\n" +
             "  **!role** -- Shows allowed roles.\n" +
-            "     !role add [@Role] -- Take an allowed role yourself.\n" +
-            "     !role remove [@Role] -- Remove an allowed role that you have.\n\n" +
+            "     !role add _Role_ -- Take an allowed role yourself.\n" +
+            "     !role remove _Role_ -- Remove an allowed role that you have.\n\n" +
             "**Developer Commands**\n\n" +
-            "  **!roleid [@Role]** -- Shows the role's ID\n" +
+            "  **!roleid _Role_** -- Shows the role's ID\n" +
             "  **!github** -- Sends GitHub repository link");
     });
 }
@@ -219,6 +205,8 @@ function OnGuildMemberRemove(member)
     const channel = member.guild.channels.get(WelcomeMessageChannelID);
     channel.send(`${member.user} çıktı.`);
 }
+
+
 
 exports.RunCommand = RunCommand;
 exports.OnGuildMemberAdd = OnGuildMemberAdd;
